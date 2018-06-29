b0fbdfeab75499fb6f6acbade10f4ab420ef9350
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-06-29 13:22:37 -0700

    Updates Book class Edits after project assessment Eliminates @@all_titles Eliminates clear @@all method Shovels book into @@all array during initialization Eliminates save method Updates downcase_all_titles method to use @@all instead of @@all_titles

--- lib/book.rb
+++ lib/book.rb
@@ -3,38 +3,23 @@ require_relative '../lib/scraper.rb'
 class DayBooks::Book
   attr_accessor :title, :book_url
   @@all = []
-  @@all_titles = []
 
   def initialize(title = nil, book_url = nil)
     @title = title
     @book_url = book_url
+    @@all << self
   end
 
   def self.all
     @@all
   end
 
-  def self.all_titles
-    @@all_titles
-  end
-
-  def save
-    @@all << self
-    @@all_titles << @title
-  end
-
-  def self.empty_all
-    @@all.clear
-    @@all_titles.clear
-  end
-
   def self.downcase_all_titles
-    @@all_titles.map {|title| title.downcase}
+    @@all.map {|book| book.title.downcase}
   end
 
   def self.create(title, book_url)
     new_book = self.new(title, book_url)
-    new_book.save
     new_book
   end
 end
