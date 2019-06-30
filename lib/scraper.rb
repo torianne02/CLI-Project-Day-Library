3899e06373e6eb461f56bc8a494e06a8943f2ffe
torianne02 <victoria.fluharty@gmail.com>
2019-06-30 13:51:09 -0700

    refactors code in scraper.rb

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -14,8 +14,7 @@ class DayBooks::Scraper
 
   def self.get_book_info
     author_library_url = "https://www.goodreads.com/author/list/19823.Sylvia_Day?page=1&per_page=99999"
-    book_info = self.get_page(author_library_url).css('tr[itemtype="http://schema.org/Book"]')
-    book_info.each do |book|
+    self.get_page(author_library_url).css('tr[itemtype="http://schema.org/Book"]').each do |book|
       @@books_array << {
         :title => book.css('.bookTitle span[itemprop=name]').text,
         :book_url => book.css('a').attribute('href').value
@@ -25,8 +24,6 @@ class DayBooks::Scraper
 
   def self.get_description(book_url)
     full_book_url = "https://www.goodreads.com" + book_url
-    doc = self.get_page(full_book_url)
-    book_description = doc.css('div#descriptionContainer span[2]').text
-    book_description
+    self.get_page(full_book_url).css('div#descriptionContainer span[2]').text
   end
 end
